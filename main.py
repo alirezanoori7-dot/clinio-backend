bc97f6e952933cc098170a2b047967d2296af0ec
alirezanoori7-dot <alirezanoori7@gmail.com>
2025-09-19 22:16:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# اضافه کردن CORS بعد از ساختن app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # برای تست ساده
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
